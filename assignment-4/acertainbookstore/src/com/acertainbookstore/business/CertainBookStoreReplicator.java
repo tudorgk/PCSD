acb680a57fa440050c9fb3718dd8f0c805dc9368
Tudor Dragan <me@tudordev.com>
2014-12-15 12:28:00 +0100

    replicator

--- assignment-4/acertainbookstore/src/com/acertainbookstore/business/CertainBookStoreReplicator.java
+++ assignment-4/acertainbookstore/src/com/acertainbookstore/business/CertainBookStoreReplicator.java
@@ -1,25 +1,51 @@
 package com.acertainbookstore.business;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
+import com.acertainbookstore.client.BookStoreClientConstants;
 import com.acertainbookstore.interfaces.Replicator;
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.util.thread.QueuedThreadPool;
 
 /**
  * CertainBookStoreReplicator is used to replicate updates to slaves
  * concurrently.
  */
 public class CertainBookStoreReplicator implements Replicator {
+   	private ExecutorService executorService;
+	private HttpClient client;
 
-	public CertainBookStoreReplicator(int maxReplicatorThreads) {
-		// TODO:Implement this constructor
+	public CertainBookStoreReplicator(int maxReplicatorThreads){
+		// create an executor service for the requests
+		executorService = Executors.newFixedThreadPool(maxReplicatorThreads);
+
+		//instantiate a client to make the requests
+		client = new HttpClient();
+		client.setConnectorType(HttpClient.CONNECTOR_SELECT_CHANNEL);
+		client.setMaxConnectionsPerAddress(maxReplicatorThreads);
+		client.setThreadPool(new QueuedThreadPool(maxReplicatorThreads));
+		client.setTimeout(BookStoreClientConstants.CLIENT_MAX_TIMEOUT_MILLISECS);
+		try {
+			client.start();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	public List<Future<ReplicationResult>> replicate(Set<String> slaveServers,
 			ReplicationRequest request) {
-		// TODO: Implement this method
-		return null;
+
+		Future futureTask;
+		List<Future<ReplicationResult>> results = new ArrayList<Future<ReplicationResult>>();
+
+		for(String slave : slaveServers){
+			futureTask = executorService.submit(new CertainBookStoreReplicationTask(request,slave,client));
+			results.add(futureTask);
+		}
+		return results;
 	}
 
 }
