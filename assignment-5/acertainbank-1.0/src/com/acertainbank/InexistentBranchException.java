48310c1c614f2f3b4b45905db7abeedfc05d75f3
Tudor Dragan <me@tudordev.com>
2015-01-06 12:52:20 +0100

    initial commit for assignment 5

---
+++ assignment-5/acertainbank-1.0/src/com/acertainbank/InexistentBranchException.java
@@ -0,0 +1,30 @@
+package com.acertainbank;
+
+/**
+ * This Exception is thrown when the branch ID
+ * with which one of the calls to AccountManager
+ * is called is not present in the system.
+ */
+public class InexistentBranchException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	private int branchId;
+	
+	public InexistentBranchException (String message, int branchId) {
+		super(message);
+		this.branchId = branchId;
+	}
+
+	public InexistentBranchException (int branchId) {
+		super("The branch "+branchId+" does not exist");
+		this.branchId = branchId;
+	}
+
+	public InexistentBranchException () {
+		super("The branch does not exist");
+	}
+
+	public int getBranchId () {
+		return branchId;
+	}
+}
